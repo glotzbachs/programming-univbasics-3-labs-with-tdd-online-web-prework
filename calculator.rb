da8bc559f43bf0f718bfd21cddab1f3c2d6d5934
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-09-16 15:52:57 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,6 @@
 first_number=8.0# Add your variables here
 second_number=4.0
 sum=first_number+second_number
-difference=first_number-secon
+difference=first_number-second_number
+product=first_number*second_number
+quotient=first_number/second_number
